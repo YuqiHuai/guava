b4456973dbf498ef8f28159571c99d3c602c4e53
YuqiHuai <34195403+YuqiHuai@users.noreply.github.com>
2020-11-05 04:14:22 +0800

    Update SampleTest.java

--- guava/src/SampleTest.java
+++ guava/src/SampleTest.java
@@ -13,31 +13,39 @@ class SampleTest {
 	@Test
 	void kIsPositiveIntger() {
 		Assertions.assertTrue(DoubleMath.isPowerOfTwo(2));  // k = 1
-		Assertions.assertTrue(DoubleMath.isPowerOfTwo(4));  // k = 2
-		Assertions.assertTrue(DoubleMath.isPowerOfTwo(8));  // k = 3
-		Assertions.assertTrue(DoubleMath.isPowerOfTwo(16)); // k = 4
+		// Alternatives
+		// Assertions.assertTrue(DoubleMath.isPowerOfTwo(4));  // k = 2
+		// Assertions.assertTrue(DoubleMath.isPowerOfTwo(8));  // k = 3
+		// Assertions.assertTrue(DoubleMath.isPowerOfTwo(16)); // k = 4
+		// TODO: substitute for Parameterized Testing for Assignment 2
 	}
 	@Test
 	void kIsPositiveDouble() {
 		Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, 1.5))); // k = 1.5
-		Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, 2.5))); // k = 2.5
-		Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, 3.5))); // k = 3.5
-		Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, 4.5))); // k = 4.5
+		// Alternatives
+		// Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, 2.5))); // k = 2.5
+		// Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, 3.5))); // k = 3.5
+		// Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, 4.5))); // k = 4.5
+		// TODO: substitute for Parameterized Testing for Assignment 2
 	}
 	
 	@Test
 	void kIsNegativeInteger() {
 		Assertions.assertTrue(DoubleMath.isPowerOfTwo(0.5)); // k = -1
-		Assertions.assertTrue(DoubleMath.isPowerOfTwo(0.25)); // k = -2
-		Assertions.assertTrue(DoubleMath.isPowerOfTwo(0.125)); // k = -3
+		// Alternatives
+		// Assertions.assertTrue(DoubleMath.isPowerOfTwo(0.25)); // k = -2
+		// Assertions.assertTrue(DoubleMath.isPowerOfTwo(0.125)); // k = -3
+		// TODO: substitute for Parameterized Testing for Assignment 2
 	}
 
 	@Test
 	void kIsNegativeDouble() {
 		Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, -1.5))); // k = - 1.5
-		Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, -2.5))); // k = - 2.5
-		Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, -3.5))); // k = - 3.5
-		Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, -4.5))); // k = - 4.5
+		// Alternatives
+		// Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, -2.5))); // k = - 2.5
+		// Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, -3.5))); // k = - 3.5
+		// Assertions.assertFalse(DoubleMath.isPowerOfTwo(Math.pow(2, -4.5))); // k = - 4.5
+		// TODO: substitute for Parameterized Testing for Assignment 2
 	}
 	
 	@Test
